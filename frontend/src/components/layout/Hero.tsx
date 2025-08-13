066111178788e9335520754f615540ed74459ee8
louisdevzz <vohuunhan1310@gmail.com>
2025-08-13 23:48:58 +0700

    feat: update ui home

--- frontend/src/components/layout/Hero.tsx
+++ frontend/src/components/layout/Hero.tsx
@@ -1,24 +1,22 @@
+import { ArrowRight,BookA } from "lucide-react";
 import { Button } from "../ui/button";
 
 export default function Hero() {
   return (
-    <div className="min-h-screen bg-white">
-      <div className="container mx-auto -mt-10 font-sora">
+    <div className="bg-white">
+      <div className="container px-6 mx-auto -mt-10 font-sora">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           <div className="space-y-4">
             <div className="text-black text-lg font-light flex gap-1">
               The <img src="/icons/world.svg" alt="world" className="w-6 h-6" /> Internet Capital Markets toolkit
             </div>
             
-            <h1 className="text-5xl lg:text-6xl font-bold text-black leading-tight">
-              Launch Tokens
-              <br />
-              Across Multiple
-              <br />
-              Chains
-            </h1>
+            <div className="text-5xl flex flex-col gap-1 lg:text-6xl font-bold text-black leading-tight">
+              <span>Launch Tokens</span>
+              <span>Across Multiple</span>
+              <span>Chains</span>
+            </div>
             
-            {/* Description */}
             <p className="text-sm text-black font-light leading-relaxed md:max-w-[550px]">
               Create, bridge, and launch tokens across{" "}
               <span className="inline-flex items-center space-x-2">
@@ -51,18 +49,18 @@ export default function Hero() {
               {" "}networks with our comprehensive no-code platform. <span className="text-gray-500 font-light">Launch in minutes, not months.</span>
             </p>
             
-            {/* Call-to-Action Buttons */}
             <div className="flex flex-col sm:flex-row gap-4">
               <Button className="bg-[#DD3345] hover:bg-red-700 text-white px-8 py-5 text-sm rounded-md transition-colors">
-                Launch Your Token →
+                <span className="font-light">Launch Your Token</span> 
+                <ArrowRight className="w-4 h-4"/>
               </Button>
               <Button variant="outline" className="border-none bg-[#eaf0f6] text-black px-8 py-5 text-sm font-normal rounded-md hover:bg-[#f2f6f9] transition-colors">
-                View Documentation
+                <span className="font-light">View Documentation</span>
+                <BookA className="w-6 h-6"/>
               </Button>
             </div>
           </div>
           
-          {/* Right Section - Hero Image */}
           <div className="flex justify-center lg:justify-end mt-5">
             <img 
               src="/hero.png" 
