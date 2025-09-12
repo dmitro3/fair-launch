e48799d101f2972041bd3d75483a3e878cb14838
louisdevzz <vohuunhan1310@gmail.com>
2025-09-13 03:03:19 +0700

    feat: update

--- backend/db/migrations/0001_lazy_la_nuit.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "tokens" RENAME COLUMN "address" TO "mint_address";--> statement-breakpoint
-ALTER TABLE "tokens" ADD COLUMN "owner" varchar(255);
--- backend/db/migrations/0002_strong_roxanne_simpson.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "token_allocations" DROP COLUMN "vesting_enabled";--> statement-breakpoint
-ALTER TABLE "tokens" DROP COLUMN "admin_controls_enabled";--> statement-breakpoint
-ALTER TABLE "tokens" DROP COLUMN "schedule_launch_enabled";--> statement-breakpoint
-ALTER TABLE "tokens" DROP COLUMN "revoke_mint_authority_enabled";--> statement-breakpoint
-ALTER TABLE "tokens" DROP COLUMN "revoke_freeze_authority_enabled";
